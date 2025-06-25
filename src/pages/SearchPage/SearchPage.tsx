eac37ec2298d5cee10a20f45bb14c1d9da48d451
elizaveetaPopova <eli.bychkova@gmail.com>
2025-06-25 16:54:55 +0300

    added SearchPage component

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,10 +1,11 @@
-import Header from "../../components/layout/Header"
-
+import RepoList from "../../components/features/RepoList"
+import styles from './styles.module.css'
 const SearchPage = () => {
   return (
-    <div>
-      <Header />
-            dd
+    <div className={styles.container}>
+      <h1 className={styles.title}>Result: 100 repositories</h1>
+      <RepoList/>
+
     </div>
   )
 }
