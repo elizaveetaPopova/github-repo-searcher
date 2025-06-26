3d3c5f70e077c638b18ab58585c5a732cd0563a8
elizaveetaPopova <eli.bychkova@gmail.com>
2025-06-26 17:48:05 +0300

    fixed styles for RepoList

--- src/components/ui/RepoList/RepoList.tsx
+++ src/components/ui/RepoList/RepoList.tsx
@@ -1,17 +1,19 @@
 import RepoCard from '../../ui/RepoCard';
 import type { Repository } from '../../../types/Repos/ReposTypes';
 
+import styles from './styles.module.css';
+
 interface RepoListProps {
   repositories: Repository[];
 }
 
 const RepoList = ({ repositories }: RepoListProps) => {
   return (
-    <>
+    <div className={styles.repoList}>
       {repositories.map((repository) => (
         <RepoCard key={repository.id} repo={repository} />
       ))}
-    </>
+    </div>
   );
 };
 export default RepoList;
