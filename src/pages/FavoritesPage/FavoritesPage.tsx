33e70caa0056f7496b4fa8995e7e09e4a9d82134
elizaveetaPopova <eli.bychkova@gmail.com>
2025-06-27 17:04:40 +0300

    Added page for favorite repos with logic for removing from favorites

---
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -0,0 +1,59 @@
+import { useNavigate } from 'react-router-dom';
+import { observer } from 'mobx-react-lite';
+import BackButton from '../../components/ui/BackButton';
+import RepoList from '../../components/ui/RepoList';
+
+import Dropdown from '../../components/ui/Dropdown';
+import type { SortOption } from '../../types/Repos/ReposTypes';
+import { options } from '../../constants/sortOptions';
+import favoritesStore from '../../store/favorites.store';
+
+import styles from './styles.module.css';
+
+const FavoritesPage = observer(() => {
+  const navigate = useNavigate();
+  const { favorites, setSortBy, sortBy } = favoritesStore;
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  const handleDropdownChange = (option: SortOption) => {
+    setSortBy(option.value);
+  };
+
+  const selectedLabel =
+    options.find((option) => option.value === sortBy)?.label || '';
+  console.log(
+    'favoritesStore.sortedFavorites :>> ',
+    favoritesStore.sortedFavorites
+  );
+  return (
+    <div className={styles.container}>
+      <BackButton onClick={goBack} />
+      {favorites.length > 0 ? (
+        <>
+          <div className={styles.reposHeader}>
+            <h2 className={styles.reposTitle}>Favorites: {favorites.length}</h2>
+            <Dropdown
+              onChange={handleDropdownChange}
+              options={options}
+              sortBy={selectedLabel}
+            />
+          </div>
+          <RepoList
+            onToggleFavorite={favoritesStore.toggleFavorite}
+            favorites={favoritesStore.sortedFavorites}
+            repositories={favoritesStore.sortedFavorites}
+          />
+        </>
+      ) : (
+        <div className={styles.reposHeader}>
+          <h2 className={styles.reposTitle}>No favorites yet</h2>
+        </div>
+      )}
+    </div>
+  );
+});
+
+export default FavoritesPage;
