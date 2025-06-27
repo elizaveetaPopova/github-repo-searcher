146273caf6ca86eb1679659fa7b8701af2af81dc
elizaveetaPopova <eli.bychkova@gmail.com>
2025-06-27 17:05:26 +0300

    added go back button

---
+++ src/components/ui/BackButton/BackButton.tsx
@@ -0,0 +1,17 @@
+import arrowLeft from '../../../assets/images/Arrow left.png';
+import styles from './styles.module.css';
+
+interface BackButtonProps {
+  onClick: () => void;
+}
+
+const BackButton = ({ onClick }: BackButtonProps) => {
+  return (
+    <button className={styles.button} onClick={onClick}>
+      <img src={arrowLeft} alt="arrow" />
+      <span>Back</span>
+    </button>
+  );
+};
+
+export default BackButton;
