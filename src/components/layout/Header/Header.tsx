b25c6790132a9dd4435cfeea6109feb03e40c72d
elizaveetaPopova <eli.bychkova@gmail.com>
2025-06-27 17:06:50 +0300

    added navigation to home and to favorites

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,31 +1,41 @@
-import logo from '../../../assets/images/logo.png'
-import heart from '../../../assets/images/heart_simple.png'
-import role from '../../../assets/images/Role.png'
+import { useNavigate } from 'react-router-dom';
+import logo from '../../../assets/images/logo.png';
+import heart from '../../../assets/images/heart_simple.png';
+import role from '../../../assets/images/Role.png';
 
-import style from './styles.module.css'
+import style from './styles.module.css';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const goToFavorites = () => {
+    navigate('/favorites');
+  };
+  const goToProfile = () => {};
+  const goToHome = () => {
+    navigate('/');
+  };
+
   return (
     <header className={style.header}>
-      <div className={style.logo}>
-        <img className={style.logo} src={logo} alt='logo' />
-      </div>
+      <button className={style.logo} onClick={goToHome}>
+        <img className={style.logo} src={logo} alt="logo" />
+      </button>
       <nav className={style.nav}>
         <ul className={style.navList}>
           <li className={style.navItem}>
-            <button className={style.navButton}>
-              <img src={heart} />
+            <button className={style.navButton} onClick={goToFavorites}>
+              <img src={heart} alt="heart" />
             </button>
           </li>
           <li className={style.navItem}>
-            <button className={style.navButton}>
-              <img src={role} />
+            <button className={style.navButton} onClick={goToProfile}>
+              <img src={role} alt="profile" />
             </button>
           </li>
         </ul>
       </nav>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
