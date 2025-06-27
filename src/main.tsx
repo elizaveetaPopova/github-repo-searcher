0cf978aaf2d3dab67a522be2f131f9b7bcbd4fcf
elizaveetaPopova <eli.bychkova@gmail.com>
2025-06-27 17:03:34 +0300

    added store for favorites and split logic. also created AppProvider for wrapping App to common Context

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import App from './App.tsx'
+import App from './App.tsx';
+import repoStore from './store/repo.store.ts';
+import favoritesStore from './store/favorites.store.ts';
+import { AppProviders } from './AppProviders.tsx';
 
-import './index.css'
-import { RepoStoreContext } from './store/index.ts'
-import repoStore from './store/repo.store.ts'
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RepoStoreContext.Provider value={repoStore}>
+    <AppProviders githubStore={repoStore} favoritesStore={favoritesStore}>
       <App />
-    </RepoStoreContext.Provider>
-  </StrictMode>,
-)
+    </AppProviders>
+  </StrictMode>
+);
